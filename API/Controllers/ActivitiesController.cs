fbd1bc603e4beeaf9918bacf276a933e015cfea7
Bryan Tee <bryan.tee@lm-c02g558emd6r.inside.xero.com>
2023-03-20 09:30:13 +1300

    Initial Commit: Skeletal Backend

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivites()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
+    }
+}
